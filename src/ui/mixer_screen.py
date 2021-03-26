5d665d411c99602e70f672f05fbfd46609db830d
Deepargh Chatterjee <54524272+jsharp008@users.noreply.github.com>
2021-03-26 22:09:35 +0530

    mixer popup

--- src/ui/mixer_screen.py
+++ src/ui/mixer_screen.py
@@ -1,6 +1,7 @@
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
+from kivy.uix.popup import Popup
 
 class MixerScreen(Screen):
 	pass
@@ -13,6 +14,24 @@ class TracksList(Widget):
 
 class TracksGrid(Widget):
     pass
+
+class TracksPopup(Popup):
+    def vibrato_frequency_slider(self, *args):
+        value = str((float(("{0:.1f}".format(args[1])))))
+        self.ids.vibratoFrequencySlider.text = "Vibrato frequency: " + value 
+
+    def vibrato_variance_slider(self, *args):
+        value = str((float(("{0:.1f}".format(args[1])))))
+        self.ids.vibratoVarianceSlider.text = "Vibrato variance: " + value 
+
+    def attack_slider(self, *args):
+        value = str((float(("{0:.3f}".format(args[1])))))
+        self.ids.attackSlider.text = "Attack: " + value 
+    
+    def decay_slider(self, *args):
+        value = str((float(("{0:.2f}".format(args[1])))))
+        self.ids.decaySlider.text = "Decay: " + value
+    #pass
 
 class GetTrackButton(Button):
     track_number = 1
