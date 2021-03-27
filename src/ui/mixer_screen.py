55788d1a16db8f8d7ff35623f0ac80b675ef2234
Deepargh Chatterjee <54524272+jsharp008@users.noreply.github.com>
2021-03-27 12:01:24 +0530

    added track popup

--- src/ui/mixer_screen.py
+++ src/ui/mixer_screen.py
@@ -2,11 +2,14 @@ from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
 class MixerScreen(Screen):
 	pass
 
 class TracksList(Widget):
+    #def tracks_list(self,*args):
+
 	pass
 
 #class TrackButton(Button):
@@ -16,30 +19,39 @@ class TracksGrid(Widget):
     pass
 
 class TracksPopup(Popup):
+    #track_number = 1
     def vibrato_frequency_slider(self, *args):
         value = str((float(("{0:.1f}".format(args[1])))))
-        self.ids.vibratoFrequencySlider.text = "Vibrato frequency: " + value 
+        self.ids.vibratoFrequencySliderText.text = "Vibrato frequency: " + value 
 
     def vibrato_variance_slider(self, *args):
         value = str((float(("{0:.1f}".format(args[1])))))
-        self.ids.vibratoVarianceSlider.text = "Vibrato variance: " + value 
+        self.ids.vibratoVarianceSliderText.text = "Vibrato variance: " + value 
 
     def attack_slider(self, *args):
         value = str((float(("{0:.3f}".format(args[1])))))
-        self.ids.attackSlider.text = "Attack: " + value 
+        self.ids.attackSliderText.text = "Attack: " + value 
     
     def decay_slider(self, *args):
         value = str((float(("{0:.2f}".format(args[1])))))
-        self.ids.decaySlider.text = "Decay: " + value
+        self.ids.decaySliderText.text = "Decay: " + value
+
+    def confirm_track(self, *args):
+        #do shit
+        #track_number += 1
+        print("track confirm")
+        #self.parent.ids.tracks_grid.add_widget(Label(text='asdasd'))
+        #MixerScreenLayout.add_widget(Button(text='heallo'))
+        self.dismiss()
     #pass
 
 class GetTrackButton(Button):
-    track_number = 1
-    def get_tracks(self):
-       self.track_number += 1
-       self.ids.tracks_grid.add_widget(Button(text='Track' + str(self.track_number)))
+    #track_number = 1
+    #def get_tracks(self):
+       #self.track_number += 1
+       #self.ids.tracks_grid.add_widget(Button(text='Track' + str(self.track_number)))
        #track_button = Button(text='Track ' + str(self.track_number)
-    #pass
+    pass
 
 class MixerScreenLayout(Widget):
     def buttonPress(self):
