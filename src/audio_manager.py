2f33c8306e2ff6816d4e1dd306eb6b96ce76b6cc
Supantha Paul <supapaul56@gmail.com>
2021-03-30 22:00:25 +0530

    some code cleanup

--- src/audio_manager.py
+++ src/audio_manager.py
@@ -22,41 +22,33 @@ class AudioManager:
 			AudioManager()
 		return AudioManager.__instance__
 
+	# creates a mixer
 	def create_mixer(self, sample_rate, amplitude):
 		self.mixer = Mixer(sample_rate, amplitude)
 		self.curr_track_id = 0
 		return self.mixer
 
+	# creates a track
 	def create_track(self, track_type, vibrato_freq, vibrato_variance, attack, decay):
 		if not self.mixer:
 			print("You need to create a Mixer first")
 			return
 
-		# if not isinstance(track_type, TrackType):
-		# 	raise TypeError('track_type must be an instance of TrackType Enum')
-		# create track
 		self.mixer.create_track(self.curr_track_id, track_type, vibrato_frequency=vibrato_freq, vibrato_variance=vibrato_variance, attack=attack, decay=decay)
 		# increment current track id
 		self.curr_track_id += 1
 		# return id of this track
 		return self.curr_track_id - 1
+
+	# adds a tone to a specified track
 	def add_tone(self, track_id, startNote, octave, duration, endNote):
 		self.mixer.add_note(track_id, note=startNote, octave=octave, duration=duration, endnote=endNote)
 		return (startNote, octave, duration, endNote)
+
+	# audio preview
 	def preview_audio(self):
 		self.mixer.write_wav('temp.wav')
 		sound = SoundLoader.load('temp.wav')
 		if sound:
 			sound.play()
 
-
-
-file_root = 'assets/sounds/'
-def play_source_audio(instance):
-	sound = SoundLoader.load(file_root + 'coin.wav')
-	if sound:
-		sound.play()
-	pass
-
-def convert_and_play(instance):
-	pass
